901886168a1cd9f7201a0bf74fb4456c76d0cae0
anastasiaspencer <spenceranastasia7@gmail.com>
2024-11-04 11:20:37 -0600

    Added Secrets manager to properly get password from Azure key manager when deployed.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 using Microsoft.EntityFrameworkCore;
 using Fall2024_Assignment3_akspencer1.Data;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var keyVaultUri = new Uri("https://akspencer1keyvault.vault.azure.net/");
+
+builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+
+var dbPassword = builder.Configuration["DatabasePassword"];
+if (string.IsNullOrEmpty(dbPassword))
+{
+    throw new InvalidOperationException("Database password could not be retrieved from Key Vault.");
+}
+
+// Add services to the container. directly grab the password and put it in here building the service
+var connectionString = $"Server=tcp:fall2024-assignment3-akspencer1.database.windows.net,1433;Initial Catalog=fall2024-assignment3-akspencer1;Persist Security Info=False;User ID=akspencer1;Password={dbPassword};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -14,6 +26,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
 
 
 
